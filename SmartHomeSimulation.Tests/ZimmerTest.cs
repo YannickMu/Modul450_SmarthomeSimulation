5feec9de5f17534e2a40b9bd591012682d2b6614
Yannick Müller <info@yannickmueller.dev>
2025-10-31 13:45:39 +0100

    add testproject
    Signed-off-by: Yannick Müller <info@yannickmueller.dev>

---
+++ SmartHomeSimulation.Tests/ZimmerTest.cs
@@ -0,0 +1,17 @@
+using JetBrains.Annotations;
+using M320_SmartHome;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SmartHomeSimulation.Tests;
+
+[TestClass]
+[TestSubject(typeof(Zimmer))]
+public class ZimmerTest
+{
+
+    [TestMethod]
+    public void TestVerarbeiteWetterdaten()
+    {
+        
+    }
+}
