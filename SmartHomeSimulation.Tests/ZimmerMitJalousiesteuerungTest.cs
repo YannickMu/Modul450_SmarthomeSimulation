f4b729165b81605b0edd042cdcb90b3154e426be
Yannick Müller <info@yannickmueller.dev>
2025-11-07 14:16:36 +0100

    feat: added first test for ZimmerMitJalousiesteuerung
    Signed-off-by: Yannick Müller <info@yannickmueller.dev>

--- SmartHomeSimulation.Tests/ZimmerMitJalousiesteuerungTest.cs
+++ SmartHomeSimulation.Tests/ZimmerMitJalousiesteuerungTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using JetBrains.Annotations;
 using M320_SmartHome;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -8,10 +10,26 @@ namespace SmartHomeSimulation.Tests;
 [TestSubject(typeof(ZimmerMitJalousiesteuerung))]
 public class ZimmerMitJalousiesteuerungTest
 {
+    private Schlafzimmer testZimmer = new Schlafzimmer();
 
     [TestMethod]
-    public void METHOD()
+    public void TestVerarbeiteWetterdaten_AussentemperaturGroesserTeperaturvorgabeUndPersonInZimmer_JalousieLassen()
     {
+        Wetterdaten wetterdaten = new Wetterdaten();
+        wetterdaten.Aussentemperatur = 25;
+        wetterdaten.Regen = false;
+        wetterdaten.Windgeschwindigkeit = 0;
         
+        ZimmerMitJalousiesteuerung testObj = new ZimmerMitJalousiesteuerung(testZimmer);
+        testObj.PersonenImZimmer = true;
+        testObj.Temperaturvorgabe = 23;
+        
+        StringWriter stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+        
+        testObj.VerarbeiteWetterdaten(wetterdaten);
+        
+        Assert.IsFalse(testObj.JalousieHeruntergefahren);
+        Assert.AreEqual($"{testObj.Name}: Jalousie kann nicht geschlossen werden weil Personen im Zimmer sind.\nWetterdaten für {testObj.Name} verarbeitet: Temperaturvorgabe: {testObj.Temperaturvorgabe}°C, Personen im Zimmer: {(testObj.PersonenImZimmer ? "ja" : "nein")}.\n", stringWriter.ToString());
     }
 }
