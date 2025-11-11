56d91788b30e21a72e9cff82c51f5d9e7311c16a
Yannick Müller <info@yannickmueller.dev>
2025-11-11 08:12:02 +0100

    feat: added first test for ZimmerMitMarkisensteuerung
    Signed-off-by: Yannick Müller <info@yannickmueller.dev>

--- SmartHomeSimulation.Tests/ZimmerMitMarkisensteuerungTest.cs
+++ SmartHomeSimulation.Tests/ZimmerMitMarkisensteuerungTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using JetBrains.Annotations;
 using M320_SmartHome;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -8,10 +10,31 @@ namespace SmartHomeSimulation.Tests;
 [TestSubject(typeof(ZimmerMitMarkisensteuerung))]
 public class ZimmerMitMarkisensteuerungTest
 {
+    Wohnzimmer testZimmer = new Wohnzimmer();
 
     [TestMethod]
-    public void METHOD()
+    public void TestVerarbeiteWetterdaten_AussentemperaturGrösserTemperaturvorgabeMarkiseOffenUndRegen_MarkiseKannNichtGeschlossenWerden()
     {
+        ZimmerMitMarkisensteuerung testObj = new ZimmerMitMarkisensteuerung(testZimmer);
+        testObj.Temperaturvorgabe = 20;
+        testObj.PersonenImZimmer = true;
         
+        Wetterdaten wetterdaten = new Wetterdaten();
+        wetterdaten.Aussentemperatur = 19;
+        wetterdaten.Regen = true;
+        wetterdaten.Windgeschwindigkeit = 0;
+        
+        StringWriter output = new StringWriter();
+        
+        testObj.VerarbeiteWetterdaten(wetterdaten);
+        
+        Assert.IsTrue(testObj.MarkiseOffen);
+        wetterdaten.Aussentemperatur = 25;
+        Console.SetOut(output);
+        
+        testObj.VerarbeiteWetterdaten(wetterdaten);
+        
+        Assert.IsTrue(testObj.MarkiseOffen);
+        Assert.AreEqual($"{testObj.Name}: Markise kann nicht geschlossen werden weils regnet.\nWetterdaten für {testObj.Name} verarbeitet: Temperaturvorgabe: {testObj.Temperaturvorgabe}°C, Personen im Zimmer: {(testObj.PersonenImZimmer ? "ja" : "nein")}.\n", output.ToString());
     }
 }
