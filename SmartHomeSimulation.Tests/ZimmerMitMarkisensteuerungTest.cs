87330ae23812eecf22b56ecb6426fa3a160c19c3
Yannick Müller <info@yannickmueller.dev>
2025-11-12 16:47:45 +0100

    feat: finished writeing tests for ZimmerMitMarkisensteuerung
    Signed-off-by: Yannick Müller <info@yannickmueller.dev>

--- SmartHomeSimulation.Tests/ZimmerMitMarkisensteuerungTest.cs
+++ SmartHomeSimulation.Tests/ZimmerMitMarkisensteuerungTest.cs
@@ -37,4 +37,73 @@ public class ZimmerMitMarkisensteuerungTest
         Assert.IsTrue(testObj.MarkiseOffen);
         Assert.AreEqual($"{testObj.Name}: Markise kann nicht geschlossen werden weils regnet.\nWetterdaten für {testObj.Name} verarbeitet: Temperaturvorgabe: {testObj.Temperaturvorgabe}°C, Personen im Zimmer: {(testObj.PersonenImZimmer ? "ja" : "nein")}.\n", output.ToString());
     }
+
+    [TestMethod]
+    public void TestVerarbeiteWetterdaten_AussentemperaturGrösserTemperaturvorgabeMarkiseOffen_MarkiseSchliessen()
+    {
+        ZimmerMitMarkisensteuerung testObj = new ZimmerMitMarkisensteuerung(testZimmer);
+        testObj.Temperaturvorgabe = 20;
+        testObj.PersonenImZimmer = true;
+        
+        Wetterdaten wetterdaten = new Wetterdaten();
+        wetterdaten.Aussentemperatur = 19;
+        wetterdaten.Regen = true;
+        wetterdaten.Windgeschwindigkeit = 0;
+        
+        StringWriter output = new StringWriter();
+        
+        testObj.VerarbeiteWetterdaten(wetterdaten);
+        
+        Assert.IsTrue(testObj.MarkiseOffen);
+        wetterdaten.Aussentemperatur = 25;
+        wetterdaten.Regen = false;
+        Console.SetOut(output);
+        
+        testObj.VerarbeiteWetterdaten(wetterdaten);
+        
+        Assert.IsFalse(testObj.MarkiseOffen);
+        Assert.AreEqual($"{testObj.Name}: Markise wird geschlossen.\nWetterdaten für {testObj.Name} verarbeitet: Temperaturvorgabe: {testObj.Temperaturvorgabe}°C, Personen im Zimmer: {(testObj.PersonenImZimmer ? "ja" : "nein")}.\n", output.ToString());
+    }
+
+    [TestMethod]
+    public void TestVerarbeiteWetterdaten_AussentemperaturKleinerTemperaturvorgabeMarkiseGeschlossenUndRegen_MarkiseWirdGeoeffnet()
+    {
+        ZimmerMitMarkisensteuerung testObj = new ZimmerMitMarkisensteuerung(testZimmer);
+        testObj.Temperaturvorgabe = 20;
+        testObj.PersonenImZimmer = true;
+        
+        Wetterdaten wetterdaten = new Wetterdaten();
+        wetterdaten.Aussentemperatur = 19;
+        wetterdaten.Regen = true;
+        wetterdaten.Windgeschwindigkeit = 0;
+        
+        StringWriter output = new StringWriter();
+        Console.SetOut(output);
+        
+        testObj.VerarbeiteWetterdaten(wetterdaten);
+        
+        Assert.IsTrue(testObj.MarkiseOffen);
+        Assert.AreEqual($"{testObj.Name}: Markise wird geöffnet.\nWetterdaten für {testObj.Name} verarbeitet: Temperaturvorgabe: {testObj.Temperaturvorgabe}°C, Personen im Zimmer: {(testObj.PersonenImZimmer ? "ja" : "nein")}.\n", output.ToString());
+    }
+
+    [TestMethod]
+    public void TestVerarbeiteWetterdaten_AussentemperaturGroesserTemperaturvorgabeMarkiseGeschlossenUndRegen_MarkiseWirdGeoeffnet()
+    {
+        ZimmerMitMarkisensteuerung testObj = new ZimmerMitMarkisensteuerung(testZimmer);
+        testObj.Temperaturvorgabe = 20;
+        testObj.PersonenImZimmer = true;
+        
+        Wetterdaten wetterdaten = new Wetterdaten();
+        wetterdaten.Aussentemperatur = 25;
+        wetterdaten.Regen = true;
+        wetterdaten.Windgeschwindigkeit = 0;
+        
+        StringWriter output = new StringWriter();
+        Console.SetOut(output);
+        
+        testObj.VerarbeiteWetterdaten(wetterdaten);
+        
+        Assert.IsTrue(testObj.MarkiseOffen);
+        Assert.AreEqual($"{testObj.Name}: Markise wird geöffnet weils regnet.\nWetterdaten für {testObj.Name} verarbeitet: Temperaturvorgabe: {testObj.Temperaturvorgabe}°C, Personen im Zimmer: {(testObj.PersonenImZimmer ? "ja" : "nein")}.\n", output.ToString());
+    }
 }
